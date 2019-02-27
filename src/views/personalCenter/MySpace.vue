5efc814c90964648b5f98413db4bbf3da46cf6df
jinwang9527 <838210051@qq.com>
2019-02-27 05:03:26 -0800

    编写动态页面

--- src/views/personalCenter/MySpace.vue
+++ src/views/personalCenter/MySpace.vue
@@ -1,1 +1,68 @@
-<template></template>
+<template>
+
+  <div>
+
+    <div>
+
+      <el-card>
+        <h3>今日头条</h3>
+        <el-carousel :interval="4000" type="card" height="300px" loop="true">
+          <el-carousel-item v-for="item in 6" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </el-card>
+    </div>
+
+    <div style="margin-top: 2%">
+      <el-card>
+        <h3>今日热门</h3>
+        <el-collapse v-model="activeNames" @change="handleChange">
+          <el-collapse-item title="一致性 Consistency" name="1">
+            <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+            <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-card>
+    </div>
+
+
+  </div>
+
+
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        activeNames: ['1']
+      };
+    },
+    methods: {
+      handleChange(val) {
+        console.log(val);
+      }
+    }
+  }
+</script>
+
+
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
